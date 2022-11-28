874cc71fc954a32bc9effb9bffeb91125c0ddc28
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-27 22:02:48 -0500

    task b

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 #include "optimism.h"
 
 int main() {
+	//TASK A
 	std::vector<int> v = makeVector(5);	
 	for (int i = 0; i < v.size(); i++) {
 		std::cout << v[i] << std::endl;
 	}
+	std::cout << "\n";
 
+	//TASK B
+	const std::vector<int> v2{1,2,-1,3,4,-1,6};
+	goodVibes(v2);
+
+	//TASK C
+	
 	return 0;
 }
--- optimism.cpp
+++ optimism.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> goodVibes(const & std::vector<int> v) {
+std::vector<int> goodVibes(const std::vector<int> &v) {
 	std::vector<int> vibes;
 	for(int i = 0; i < v.size(); i++) {
-		vibes.push_back(v[i]);
+		if(v[i] > 0) {
+			vibes.push_back(v[i]);
+		}
 	}
 
 	return vibes;
