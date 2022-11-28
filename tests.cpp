e7328f587b7851cb36a7eb933ee7b65f560ac22e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-27 22:33:35 -0500

    adding file

---
+++ tests.cpp
@@ -0,0 +1,24 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "vectors.h"
+#include "optimism.h"
+#include "fusion.h"
+#include "pairwise.h"
+#include <vector>
+
+TEST_CASE("TASK A") {
+	makeVector(5);
+	
+}
+
+TEST_CASE("TASK B") {
+
+}
+
+TEST_CASE("TASK C") {
+
+}
+
+TEST_CASE("TASK D") {
+
+}
